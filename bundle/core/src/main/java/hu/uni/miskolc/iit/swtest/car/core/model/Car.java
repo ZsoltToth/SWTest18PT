07b9252d936bf510da6ae1bf72b27cb9b08ec654
Zsolt Toth <tothzs@iit.uni-miskolc.hu>
2018-09-14 15:26:43 +0200

    Car class has been added. InvalidProductionYearException has been added.

--- bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/car/core/model/Car.java
+++ bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/car/core/model/Car.java
@@ -2,4 +2,41 @@ package hu.uni.miskolc.iit.swtest.car.core.model;
 
 public class Car {
 
+	private Manufacturer manufacturer;
+	private CarModel model;
+	private String color;
+	private int productionYear;
+	private int grossWeight;
+
+	public Car(CarModel model, String color, int productionYear, int grossWeight) throws InvalidProductionYearException{
+		super();
+		if(productionYear < 1900) {
+			throw new InvalidProductionYearException();
+		}
+		this.model = model;
+		this.color = color;
+		this.productionYear = productionYear;
+		this.grossWeight = grossWeight;
+	}
+
+	public Manufacturer getManufacturer() {
+		return manufacturer;
+	}
+
+	public CarModel getModel() {
+		return model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getProductionYear() {
+		return productionYear;
+	}
+
+	public int getGrossWeight() {
+		return grossWeight;
+	}
+
 }
---
+++ bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/car/core/model/InvalidProductionYearException.java
@@ -0,0 +1,30 @@
+package hu.uni.miskolc.iit.swtest.car.core.model;
+
+public class InvalidProductionYearException extends Exception {
+
+	public InvalidProductionYearException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidProductionYearException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidProductionYearException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidProductionYearException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidProductionYearException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
