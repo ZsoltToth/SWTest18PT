97febb478a4a0d8cad8f555fa82dc4e5f79467c5
Zsolt Toth <tothzs@iit.uni-miskolc.hu>
2018-09-14 14:33:09 +0200

    core module has been added

---
+++ bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/car/core/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.iit.swtest.car.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
